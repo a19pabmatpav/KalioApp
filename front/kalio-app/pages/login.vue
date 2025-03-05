f246418307d00b0576dcaac2f82e860a2e218e1d
a19pabmatpav <a19pabmatpav@inspedralbes.cat>
2025-03-05 16:11:11 +0100

    instslando laravel

--- front/kalio-app/pages/login.vue
+++ front/kalio-app/pages/login.vue
@@ -1,18 +1,9 @@
 <script setup>
-definePageMeta({
-  layout: 'admin'
-})
-
-const greeting = ref('Hello World!')
 
 </script>
 
 <template>
-  <p class="greeting">{{ greeting }}</p>
-
   
-y aqui un ComponentPublicInstance
-<helloWorld />
 </template>
 
 <style>
