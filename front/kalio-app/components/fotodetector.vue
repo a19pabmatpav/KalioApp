97f8c8cf072efa764cd62d765025352231ed9645
a19pabmatpav <a19pabmatpav@inspedralbes.cat>
2025-04-08 08:41:45 +0200

    trabajando en el  template

---
+++ front/kalio-app/components/fotodetector.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="detector">
+      <h1>Afegir consum</h1>
+      
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'fotodetector',
+    data() {
+      return {
+        alimento: '',
+        alimentos: [],
+        liquido: 'agua',
+        liquidos: [],
+      };
+    },
+    methods: {
+      agregarAlimento() {
+        this.alimentos.push({
+          alimento: this.alimento,
+        });
+        this.alimento = '';
+      },
+      agregarLiquido() {
+        this.liquidos.push({
+          liquido: this.liquido,
+          cantidad: this.cantidadLiquido,
+        });
+        this.liquido = 'agua';
+        this.cantidadLiquido = 0;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .detector {
+    color: white;
+    max-width: 77vw;
+    max-height: 75vh;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #3C3145;
+    background-color: #3C3145;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 75vh;
+  }
+  
+  .detector h1 {
+    color: white;
+    font-size: 2em;
+    margin-bottom: 20px;
+  }
+  
+  .detector button {
+    background-color: #3C3145;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 1em;
+  }
+  
+  .detector button:hover {
+    background-color: #5A4B8A;
+  }
+  </style>
+  
--- front/kalio-app/pages/addConsum.vue
+++ front/kalio-app/pages/addConsum.vue
@@ -1,26 +1,48 @@
 <template>
-    <div>
-        <addConsumForm />
-    </div>
+  <div>
+    <h1>Afegir consum</h1>
+    <button @click=formulari>Afegir manualment</button>
+    <button @click=detector>Fotodetector</button>
+  </div>
+  <div>
+    <addConsumForm v-if:actiu="formulari" />
+    <fotodetector v-if:actiu="detector" />
+  </div>
 </template>
 
 <script>
 definePageMeta({
   middleware: 'auth'
 })
+
+export default {
+  data() {
+    return {
+      actiu: '',
+    }
+  },
+  methods: {
+    formulari() {
+      this.actiu = 'formulari';
+    },
+    detector() {
+      this.actiu = 'fotodetector';
+    }
+  }
+
+}
+
 </script>
 
 <style scoped>
-
-
 div {
-    color : white;
+  color: white;
   max-width: 77vw;
   max-height: 75vh;
   margin: 0 auto;
   padding: 20px;
   border: 1px solid #3C3145;
-  background-color:  #3C3145;
+  background-color: #3C3145;
   border-radius: 5px;
   display: flex;
   flex-direction: column;
